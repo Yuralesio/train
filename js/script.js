e73726f2d0c8aac512195fb495e474c6ddb9efb5
YUrii <yurii.lesio@gmail.com>
2020-11-02 20:59:30 +0200

    “Update6”

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function (){
+$('.slider').slick({
+    arrows:true,
+    dots:true,
+    adaptiveHeight: true,
+    speed:1000,
+    autoplay:true,
+    autoplaySpeed: 2000,
+    pauseOnHover: false,
+    draggable:false,
+    });
+});
